8b31ac8c64376b3403fc9ea98d4c71372d17fc79
Matt Haffner <mrhaffner13@gmail.com>
2021-01-29 01:08:37 -0600

    updated expo to fix Picker related bug in android. Added platform specific padding to RepositoryListDropDown

--- src/components/Repository/RepositoryListDropdown.jsx
+++ src/components/Repository/RepositoryListDropdown.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import RNPickerSelect from 'react-native-picker-select';
+import { StyleSheet, View, Platform } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        padding: Platform.select({
+            android: 5,
+            ios: 10,
+            default: 10,
+        }),
+    },  
+});
 
 const RepositoryListDropdown = ({setListOrder}) => {
     return (
-        <RNPickerSelect
-            onValueChange={(value) => setListOrder(value)}
-            items={[
-                { label: 'Latest repositories', value: 'latest' },
-                { label: 'Highest rated repositories', value: 'highest' },
-                { label: 'Lowest rated repositories', value: 'lowest' },
-            ]}
-            placeholder={{}}
-        />
+        <View style={styles.container}>
+            <RNPickerSelect
+                onValueChange={(value) => setListOrder(value)}
+                items={[
+                    { label: 'Latest repositories', value: 'latest' },
+                    { label: 'Highest rated repositories', value: 'highest' },
+                    { label: 'Lowest rated repositories', value: 'lowest' },
+                ]}
+                placeholder={{}}
+            />
+        </View>
     );
 };
 
